2d4bde8a27ac384ee579f2ef9fe098463887f67f
Vlad <vladchukaev@gamil.com>
2022-07-08 21:37:08 +0500

    Delete Boid.hpp and comments

--- boids/main.cpp
+++ boids/main.cpp
@@ -1,21 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <fstream>
 #include <vector>
 
 class Bird {
     public:
         Bird(){
-            this->x = rand() % 700 + 0;
-            this->y = rand() % 500 + 0;
-            this->dx = 3;
-            this->dy = -3;
+            x = rand() % 700 + 0;
+            y = rand() % 500 + 0;
+            dx = 3;
+            dy = -3;
         
-            this->triangle.setRadius(15);
-            this->triangle.setPointCount(3);
-            this->triangle.setOutlineColor(sf::Color::Blue);
-            this->triangle.setOutlineThickness(2.f);
-            this->triangle.setFillColor(sf::Color::Green);
+            triangle.setRadius(15);
+            triangle.setPointCount(3);
+            triangle.setOutlineColor(sf::Color::Blue);
+            triangle.setOutlineThickness(2.f);
+            triangle.setFillColor(sf::Color::Green);
         }
 
         sf::CircleShape getBird(){
@@ -61,25 +60,12 @@ int main()
     int height = 600;
     int width = 800;
     sf::RenderWindow window(sf::VideoMode(width, height), "Boids");
-
-    // sf::CircleShape triangle(15, 3);
-    // std::vector<sf::CircleShape> boids;
-
-    // triangle.setOutlineColor(sf::Color::Blue);
-    // triangle.setOutlineThickness(2.f);
-    // triangle.setPosition(width/2, height/2);
     window.setFramerateLimit(60);
     
-    // triangle.setFillColor(sf::Color::Green);
-    sf::Clock clock;
     Bird boid;
 
     while (window.isOpen())
     {
-        float time = clock.getElapsedTime().asMicroseconds();
-        clock.restart();
-        time = time / 800;
-
         sf::Event event;
         while (window.pollEvent(event))
         {
