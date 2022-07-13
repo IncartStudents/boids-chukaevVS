b69f436bdd473411271b998541ce393bbafc833c
Vlad <vladchukaev@gamil.com>
2022-07-13 19:06:41 +0500

    Теперь птички знаю расстояние друг от друга

--- boids/main.cpp
+++ boids/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <time.h>
+#include <cmath>
 
 class Bird {
     public:
@@ -10,6 +11,8 @@ class Bird {
             y = rand() % 500 + 0;
             dx = rand() % 3 + (-3);
             dy = rand() % 3 + (-3);
+            radius = 50;
+            accelerationVec = sf::Vector2f(dx, dy);
         
             triangle.setRadius(15);
             triangle.setPointCount(3);
@@ -41,25 +44,54 @@ class Bird {
             y += dy;
             triangle.setPosition(sf::Vector2f(x, y));
         }
+
         void setX(float newX) {
             x = newX;
         }
 
+        void renderVec(sf::Vector2f position) {
+        }
+
+        sf::Vector2f getPos() {
+            return sf::Vector2f(x, y);
+        }
+
+        void setColor() {
+            this->triangle.setFillColor(sf::Color::Red);    
+        }
+
+        sf::Vector2f getAccelerationVec() {
+            return sf::Vector2f(dx, dy);
+        }
+
+        void setAccelerationVec(sf::Vector2f newAcceleration) {
+            dx = newAcceleration.x;
+            dy = newAcceleration.y;
+        }
+
+
     private:
         float x, y, dx, dy, tempX, tempY;
+        float radius;
+        sf::Vector2f accelerationVec;
         sf::CircleShape triangle;
     
 };
 
+void calcDist();
+
 int main()
 {
+    // sf::CircleShape rad(50, 300);
+    // rad.setPosition(100, 100);
+    // rad.setFillColor(sf::Color::White);
     srand(time(0));
     int height = 600;
     int width = 800;
     sf::RenderWindow window(sf::VideoMode(width, height), "Boids");
     window.setFramerateLimit(60);
 
-    int countOfBoids = 20; // Set count of boids
+    int countOfBoids = 50; // Set count of boids
 
     Bird *flock = new Bird[countOfBoids];
     for (int i = 0; i < countOfBoids; i++)
@@ -74,6 +106,28 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+        }
+
+        for (int i = 0; i < countOfBoids; i++) {
+            std::vector<sf::Vector2f> vectors;
+            int birdsInRadius = 0;
+            Bird mainBird = flock[i];
+            sf::Vector2f vecOfAcceleration = mainBird.getAccelerationVec();
+            for (int j = 0; j < countOfBoids; j++) {
+                if (mainBird.getPos() != flock[j].getPos()) {
+                    float dist = sqrt(pow(mainBird.getPos().x - flock[j].getPos().x, 2) + 
+                    pow(mainBird.getPos().y - flock[j].getPos().y, 2));
+                    
+                    if (dist < 50 && vectors.size() == 3) {
+                        for (sf::Vector2f vec : vectors) {
+                            vecOfAcceleration += vec;
+                        }
+                    } else if (dist < 50) {
+                        vectors.push_back(flock[j].getPos());
+                    }
+                }
+            }
+            flock[i].setAccelerationVec(vecOfAcceleration);
         }
 
         for (int i = 0; i < countOfBoids; i++)
@@ -91,4 +145,26 @@ int main()
     }
     delete[] flock;
     return 0;
+}
+
+void calcDist(Bird *flock, int countOfBird) {
+    for (int i = 0; i < countOfBird; i++) {
+        std::vector<Bird> flockmates;
+        int birdsInRadius = 0;
+        Bird mainBird = flock[i];
+        for (int j = 0; j < countOfBird; j++) {
+            if (mainBird.getPos() != flock[j].getPos()) {
+                float dist = sqrt(pow(mainBird.getPos().x - flock[j].getPos().x, 2) - 
+                pow(mainBird.getPos().y - flock[j].getPos().y, 2));
+
+                if (dist < 50 && birdsInRadius == 3) {
+                    mainBird.setColor();
+                } else if (dist < 50) {
+                    birdsInRadius++;
+                }
+            }
+            
+        }
+
+    }
 }
